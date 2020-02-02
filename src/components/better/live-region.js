426cfb2a8d7b569d5761ac35c83b506c1ab34e26
Andrew Hope <hope.andrew08@gmail.com>
2020-02-02 14:21:45 -0800

    Live Regions
    Signed-off-by: Andrew Hope <hope.andrew08@gmail.com>

--- src/components/better/live-region.js
+++ src/components/better/live-region.js
@@ -1,11 +1,38 @@
-import React, {useState, useRef} from "react"
+import React, { useState, useRef } from "react";
 
 const LiveRegion = () => {
+  const [message, setMessage] = useState("");
+  const inputRef = useRef(null);
+
+  const start = () => {
+    let typedMessage = inputRef.current.value;
+
+    if (typedMessage.trim().length) {
+      setMessage(typedMessage);
+    }
+  };
+
+  const submitHandler = event => {
+    event.preventDefault();
+    start();
+  };
 
   return (
-    <>
-    </>
-  )
-}
+    <form onSubmit={submitHandler}>
+      <div role="alert" id="region">
+        <p>{message ? message : ""}</p>
+      </div>
+      <label>
+        Type some text <br />
+        <input type="text" ref={inputRef} />
+      </label>
+      <div>
+        <button type="submit" onClick={start}>
+          Start
+        </button>
+      </div>
+    </form>
+  );
+};
 
-export default LiveRegion
+export default LiveRegion;
