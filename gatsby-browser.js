77add33d99e22bf9de5ec1b53bcceefdf0644d91
Andrew Hope <hope.andrew08@gmail.com>
2020-02-02 14:22:19 -0800

    Client Side routing
    Signed-off-by: Andrew Hope <hope.andrew08@gmail.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,26 @@
+let liveRegion = null;
+
 exports.onRouteUpdate = ({ location, prevLocation }) => {
-   
-}
+  if (prevLocation !== null) {
+    const skipLink = document.querySelector(".route.SkipLink");
+
+    if (skipLink) {
+      skipLink.focus();
+    }
+
+    if (location.pathname === "/") {
+      liveRegion.textContent = "home page";
+    } else {
+      liveRegion.textContent = location.pathname.split("/")[1] + " page";
+    }
+  }
+};
+
+exports.onClientEntry = () => {
+  liveRegion = document.createElement("div");
+  liveRegion.setAttribute("role", "status");
+  liveRegion.classList.add("visually-hidden");
+  liveRegion.id = "routing-region";
+
+  document.body.appendChild(liveRegion);
+};
--- src/components/better/route-target-heading.js
+++ src/components/better/route-target-heading.js
@@ -1,30 +1,38 @@
-import React from "react"
-import { css } from "@emotion/core"
+import React from "react";
+import { css } from "@emotion/core";
 
 const styles = css`
-    .routeSkipHeading {
-        position: relative;
-    }
-    .routeSkipLink {
-        display: inline-block;
-        margin-left: -0.75em;
-        opacity: 0;
-        position: absolute;
-        text-decoration: none;
-    }
-    .routeSkipLink:before {
-        content: '⇽';
-        display: block;
-    }
-    .routeSkipLink:focus,
-    .routeSkipLink:hover {
-        opacity: 1;
-    }
-`
-const RouteHeading = ({children}) => {
-    return (
-        <>
-        </>
-    )
-}
-export default RouteHeading
+  .routeSkipHeading {
+    position: relative;
+  }
+  .routeSkipLink {
+    display: inline-block;
+    margin-left: -0.75em;
+    opacity: 0;
+    position: absolute;
+    text-decoration: none;
+  }
+  .routeSkipLink:before {
+    content: "⇽";
+    display: block;
+  }
+  .routeSkipLink:focus,
+  .routeSkipLink:hover {
+    opacity: 1;
+  }
+`;
+const RouteHeading = ({ level = 2, targetID, children }) => {
+  const Heading = `h${level}`;
+  return (
+    <Heading css={styles} className="routeSkipHeading">
+      <a
+        href={`#${targetID}`}
+        id="skip-main"
+        className="routeSkipLink"
+        aria-label={`skip to ${targetID}`}
+      ></a>
+      {children}
+    </Heading>
+  );
+};
+export default RouteHeading;
