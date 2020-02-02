8edf74b60c120da3b2dbb9f695c581ca1ac17de4
Andrew Hope <hope.andrew08@gmail.com>
2020-02-02 14:21:28 -0800

    focus managed dropdown
    Signed-off-by: Andrew Hope <hope.andrew08@gmail.com>

--- src/components/better/dropdown.js
+++ src/components/better/dropdown.js
@@ -1,15 +1,74 @@
-import React, { useState, useRef, useEffect } from "react"
-import uuid from "uuid"
+import React, { useState, useRef, useEffect } from "react";
+import uuid from "uuid";
 
-import "./dropdown.scss"
+import "./dropdown.scss";
 
-const Dropdown = ({ }) => {
-    return (
-        <div
-            className="dropdown-wrap"
-        >
+const Dropdown = ({ activatorText = "Dropdown", items = [] }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const activatorRef = useRef(null);
+  const dropdownListRef = useRef(null);
 
-        </div>
-    )
-}
-export default Dropdown
+  const clickHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const wrapKeyHandler = event => {
+    if (event.key === "Escape" && isOpen) {
+      setIsOpen(false);
+      activatorRef.current.focus();
+    }
+  };
+
+  const clickOutsideHandler = event => {
+    if (
+      dropdownListRef.current.contains(event.target) ||
+      activatorRef.current.contains(event.target)
+    ) {
+      return;
+    }
+    setIsOpen();
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mouseup", clickOutsideHandler);
+      dropdownListRef.current.querySelector("a").focus();
+    } else {
+      document.removeEventListener("mouseup", clickOutsideHandler);
+    }
+
+    return () => {
+      document.removeEventListener("mouseup", clickOutsideHandler);
+    };
+  }, [isOpen]);
+
+  return (
+    <div className="dropdown-wrap" onKeyUp={wrapKeyHandler}>
+      <button
+        aria-haspopup="true"
+        aria-controls="dropdown1"
+        onClick={clickHandler}
+        className="dropdown-activator"
+        ref={activatorRef}
+      >
+        {activatorText}
+      </button>
+      <ul
+        id="dropdown1"
+        ref={dropdownListRef}
+        tabIndex="-1"
+        className={`dropdown-itemList ${isOpen ? "active" : ""}`}
+        role="list"
+      >
+        {items.map((item, index) => {
+          return (
+            <li key={index} role="listitem">
+              <a href={item.url}>{item.text}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+export default Dropdown;
