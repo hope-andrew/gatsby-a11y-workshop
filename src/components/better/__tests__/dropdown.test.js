d900512909637af97caf104142ad85898ef12095
Andrew Hope <hope.andrew08@gmail.com>
2020-02-04 10:05:23 -0800

    fix card flip animation

--- src/components/better/__tests__/dropdown.test.js
+++ src/components/better/__tests__/dropdown.test.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
 
-import Dropdown from '../dropdown'
+import Dropdown from "../dropdown";
 
 describe(`Dropdown`, () => {
   it(`renders activatorText`, () => {
-    const activatorText = `Hamburgers`
-    const { getByText } = render(<Dropdown activatorText={activatorText} />)
+    const activatorText = `Hamburgers`;
+    const { getByText } = render(<Dropdown activatorText={activatorText} />);
 
-    const text = getByText(activatorText)
+    const text = getByText(activatorText);
 
-    expect(text).toBeInTheDocument()
-  })
-})
+    expect(text).toBeInTheDocument();
+  });
+
+  it(`renders a focusable button that acttivates the dropdown`, () => {
+    const activatorText = `Dogs`;
+
+    const items = [
+      {
+        text: "item1",
+        url: "#"
+      },
+      {
+        text: "item2",
+        url: "#"
+      },
+      {
+        text: "item3",
+        url: "#"
+      }
+    ];
+  });
+});
